39e9c205ccbc36a50df17aaa4024e1842056154a
Andre Souza <andre.dsldr@gmail.com>
2021-09-19 13:31:27 -0300

    Exercise 1.1

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,27 +1,49 @@
 import React from 'react'
 
+const Header = (props) => {
+  return (
+    <div>
+      {props.course}
+    </div>
+  )
+}
+
+
+
+const Content = (props) => {
+  return (
+    <>
+      {props.exercise} {props.numberofexercises}
+    </>
+  )
+}
+
+
+
+const Total = (props) => {
+  return (
+    <>
+      {parseInt(props.ex1) + parseInt(props.ex2) + parseInt(props.ex3)}
+    </>
+  )
+}
+
+
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
 
   return (
     <div>
-      <h1>{course}</h1>
+      <h1><Header course="Half Stack application development"/></h1>
       <p>
-        {part1} {exercises1}
+        <Content exercise="Fundamentals of React" numberofexercises="10"/>
       </p>
       <p>
-        {part2} {exercises2}
+      <Content exercise="Fundamentals of React" numberofexercises="7"/>
       </p>
       <p>
-        {part3} {exercises3}
+       <Content exercise="Fundamentals of React" numberofexercises="14"/>
       </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <p>Number of exercises <Total ex1="10" ex2="7" ex3="14"/></p>
     </div>
   )
 }
